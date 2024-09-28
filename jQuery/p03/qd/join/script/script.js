d33d4cef4836edb42ec43560bd478d7fdbec3c32
silverkse <rlatjddmsrla@naver.com>
2024-09-28 17:30:11 +0900

    20240927

---
+++ jQuery/p03/qd/join/script/script.js
@@ -0,0 +1,29 @@
+$(function() {
+
+  $("div#header input[type=checkbox]").click(function(){
+    let isChecked = $(this).prop("checked");
+    $("div.chkArea input[type=checkbox]").prop("checked", isChecked);
+  });
+
+  
+  $("div.chkArea input[type=checkbox]").click(function(){
+    let chkBox1 = $("div.acceptList:eq(0) .chkArea input[type=checkbox]").prop("checked");
+    let chkBox2 = $("div.acceptList:eq(1) .chkArea input[type=checkbox]").prop("checked");
+    let chkBox3 = $("div.acceptList:eq(2) .chkArea input[type=checkbox]").prop("checked");
+    if (!chkBox1 || !chkBox2 || !chkBox3 ) {
+      $("div#header input[type=checkbox]").prop("checked", false);
+    } else if (chkBox1 && chkBox2 && chkBox3) {
+      $("div#header input[type=checkbox]").prop("checked", true);
+    }
+  });
+
+  $("div#btnArea button").click(function(){
+    let chkBox1 = $("div.acceptList:eq(0) .chkArea input[type=checkbox]").prop("checked");
+    let chkBox2 = $("div.acceptList:eq(1) .chkArea input[type=checkbox]").prop("checked");
+    if(!chkBox1 || !chkBox2) {
+      alert("필수 약관 동의 필요");
+    }
+  });
+
+
+});
